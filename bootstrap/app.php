05a098d08370dcde5de64a5db3f649c27a4ef3b9
Алексей <mail@ashatrov.ru>
2025-05-24 11:26:00 +0300

    first commit

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,19 +1,22 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Psr\Log\LogLevel;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
         api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $exceptions->render(function (\Throwable $exception, Request $request) {
+            if ($request->is('api/*'))
+                return response()->json(['error' => $exception->getMessage(),], 400);
+        });
     })->create();
